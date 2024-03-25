f8c4d034236672ed3ae2e6c313982dfc0a172386
Hussein <41930483@students.liu.edu.lb>
2024-03-25 14:33:39 +0200

    Add missing columns to Users and User tables

---
+++ Trader.EF/Migrations/20240325121816_addMissingColumnstoUsers.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Trader.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class addMissingColumnstoUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DateProcessed",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateProcessed",
+                table: "Users");
+        }
+    }
+}
