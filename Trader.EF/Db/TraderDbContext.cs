ce68967a78183f9f8e81af2a9f5a2eaf3843cf3d
Hussein <41930483@students.liu.edu.lb>
2024-03-25 14:33:25 +0200

    Refactor TraderDbContext class
    This commit refactors the TraderDbContext class in the Trader.EF.Db namespace. The changes include:
    - Adding necessary using statements
    - Modifying the OnModelCreating method to configure the AssetTransactions entity
    - Modifying the OnConfiguring method to set up the connection string

--- Trader.EF/Db/TraderDbContext.cs
+++ Trader.EF/Db/TraderDbContext.cs
@@ -5,20 +5,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Trader.Domain.Models;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Trader.EF.Db
 {
     public class TraderDbContext:DbContext
     {
-        public TraderDbContext(DbContextOptions<TraderDbContext> options) : base(options)
-        {
-        }
-
         public DbSet<Account> Accounts { get; set; }
         public DbSet<AssetTransactions> AssetTransactions { get; set; }
         public DbSet<User> Users { get; set; }
-        public DbSet<Stock> Stocks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AssetTransactions>().OwnsOne(s=>s.Stock);
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=TraderDb;Trusted_Connection=True;TrustServerCertificate = True;");
+            base.OnConfiguring(optionsBuilder);
+        }
     }
-    
    
 }
