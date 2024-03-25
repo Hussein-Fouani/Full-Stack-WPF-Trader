4c928573b980164674f564a7fced623279d38275
Hussein <41930483@students.liu.edu.lb>
2024-03-25 14:00:37 +0200

    Add TraderDbContext class

---
+++ Trader.EF/Db/TraderDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trader.Domain.Models;
+
+namespace Trader.EF.Db
+{
+    public class TraderDbContext:DbContext
+    {
+        public TraderDbContext(DbContextOptions<TraderDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<AssetTransactions> AssetTransactions { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Stock> Stocks { get; set; }
+    }
+    
+   
+}
