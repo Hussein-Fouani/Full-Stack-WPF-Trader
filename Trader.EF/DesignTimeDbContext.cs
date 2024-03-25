ab6f48c8060649fcf3bf1e9c13b7dab45a11bc83
Hussein <41930483@students.liu.edu.lb>
2024-03-25 15:39:29 +0200

    Add DesignTimeDbContext class

---
+++ Trader.EF/DesignTimeDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trader.EF.Db;
+
+namespace Trader.EF
+{
+    public class DesignTimeDbContext : IDesignTimeDbContextFactory<TraderDbContext>
+    {
+        public TraderDbContext CreateDbContext(string[] args=null)
+        {
+            var options = new DbContextOptionsBuilder<TraderDbContext>()
+                .UseSqlServer("Server=.;Database=TraderDb;Trusted_Connection=True;TrustServerCertificate = True;");
+                return new TraderDbContext(options.Options);
+        }
+    }
+}
