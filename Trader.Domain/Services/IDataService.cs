46c15d8fd4aa0ea3157a7abf40e0684e46afe5ee
Hussein <41930483@students.liu.edu.lb>
2024-03-25 13:55:33 +0200

    Add IDataService interface to Trader.Domain.Services

---
+++ Trader.Domain/Services/IDataService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trader.Domain.Services
+{
+    public interface IDataService<T>where T : class
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> Get(Guid id);
+        Task<T> Create(T entity);
+        Task<T> Update(Guid id, T entity);
+        Task<bool> Delete(Guid id);
+    }
+}
