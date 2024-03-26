06d12b93c3148811896c65955e17bf356cff40ff
Hussein <41930483@students.liu.edu.lb>
2024-03-26 16:43:07 +0200

    Add MainViewModel class

---
+++ TraderWpf/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TraderWpf.State.Navigators;
+
+namespace TraderWpf.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public INavigator Navigator { get; set; } = new Navigator();
+    }
+}
