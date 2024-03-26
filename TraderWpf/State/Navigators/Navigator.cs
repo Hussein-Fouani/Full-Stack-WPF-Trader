8a153abb7c03a09bcec179f3ff21aa36227ec4b3
Hussein <41930483@students.liu.edu.lb>
2024-03-26 16:42:44 +0200

    Add Navigator class to handle view model navigation

---
+++ TraderWpf/State/Navigators/Navigator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TraderWpf.Commands;
+using TraderWpf.Models;
+using TraderWpf.ViewModels;
+
+namespace TraderWpf.State.Navigators
+{
+    public class Navigator : ObservableObject ,INavigator
+    {
+        private ViewModelBase currentViewModel;
+        public ViewModelBase CurrentViewModel
+        {
+            get => currentViewModel;
+            set
+            {
+                currentViewModel = value;
+                OnPropertyChanged(nameof(CurrentViewModel));
+                
+            }
+        }
+
+        public ICommand UpdateCurrentViewModelCommand => new UpdateViewModelCommand(this);
+
+       
+    }
+}
