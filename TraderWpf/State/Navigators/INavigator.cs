4fd4903e718c7bfb60c240ad8845a60d9d83fbd8
Hussein <41930483@students.liu.edu.lb>
2024-03-26 16:42:39 +0200

    Add INavigator interface for managing view navigation

---
+++ TraderWpf/State/Navigators/INavigator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TraderWpf.ViewModels;
+
+namespace TraderWpf.State.Navigators
+{
+    public enum ViewType
+    {
+        Home,
+        Portfolio,
+        Buy,
+    }
+    public interface INavigator
+    {
+       
+        ViewModelBase CurrentViewModel { get; set; }
+        ICommand UpdateCurrentViewModelCommand { get; }
+
+
+
+    }
+}
