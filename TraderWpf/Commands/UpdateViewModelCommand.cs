983769fb933538715451d59a6e1bba44f5ff47f8
Hussein <41930483@students.liu.edu.lb>
2024-03-26 16:42:14 +0200

    Add UpdateViewModelCommand class

---
+++ TraderWpf/Commands/UpdateViewModelCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TraderWpf.State.Navigators;
+using TraderWpf.ViewModels;
+using static TraderWpf.State.Navigators.INavigator;
+
+namespace TraderWpf.Commands
+{
+    public class UpdateViewModelCommand : ICommand
+    {
+        private readonly INavigator navigator;
+        public event EventHandler? CanExecuteChanged;
+
+        public UpdateViewModelCommand(INavigator navigator)
+        {
+            this.navigator = navigator;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            if(parameter is ViewType)
+            {
+                ViewType viewType = (ViewType)parameter;
+                navigator.CurrentViewModel = viewType switch
+                {
+                    ViewType.Home => new HomeViewModel(),
+                    ViewType.Portfolio => new PortfolioViewModel(),
+                    ViewType.Buy => new BuyViewModel(),
+                    _ => throw new NotImplementedException(),
+                };
+            }
+        }
+    }
+}
