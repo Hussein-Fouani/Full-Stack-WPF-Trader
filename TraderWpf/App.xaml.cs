551893e79b176d26101d7466a5b4eae9a12a05f0
Hussein <41930483@students.liu.edu.lb>
2024-03-26 16:41:36 +0200

    Add App.xaml.cs file with startup logic

---
+++ TraderWpf/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using TraderWpf.ViewModels;
+
+namespace TraderWpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Window window = new MainWindow();
+            window.DataContext = new MainViewModel();
+            window.Show();
+            base.OnStartup(e);
+        }
+    }
+
+}
