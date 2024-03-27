d171030eeb215bb38d74784ca5a3778190665749
Hussein <41930483@students.liu.edu.lb>
2024-03-27 13:47:25 +0200

    Add MajorIndexViewModel to HomeViewModel constructor

--- TraderWpf/ViewModels/HomeViewModel.cs
+++ TraderWpf/ViewModels/HomeViewModel.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Trader.API.Services;
 
 namespace TraderWpf.ViewModels
 {
      public class HomeViewModel:ViewModelBase
     {
+        public MajorIndexViewModel viewModel { get; set; }
+        public HomeViewModel(MajorIndexViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+        }
     }
 }
