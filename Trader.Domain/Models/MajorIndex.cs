8e00959d993b84de4605add01f9d033c032e07ea
Hussein <41930483@students.liu.edu.lb>
2024-03-27 13:26:04 +0200

    Add MajorIndex class and MajorIndexType enum

---
+++ Trader.Domain/Models/MajorIndex.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trader.Domain.Models
+{
+    public enum MajorIndexType
+    {
+        DowJones,
+        Nasdaq,
+        SP500
+    }
+    public class MajorIndex
+    {
+        public MajorIndexType Type { get; set; }
+        public string Name { get; set; }
+        public double Changes { get; set; }
+    }
+}
