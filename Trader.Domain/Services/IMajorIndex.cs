46166f1e5c11729520842b59ef20f458121209ef
Hussein <41930483@students.liu.edu.lb>
2024-03-27 13:26:10 +0200

    Add IMajorIndex interface

---
+++ Trader.Domain/Services/IMajorIndex.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trader.Domain.Models;
+
+namespace Trader.Domain.Services
+{
+    public interface IMajorIndex
+    {
+        Task<MajorIndex> GetMajorIndex(MajorIndexType indexType);
+    }
+}
