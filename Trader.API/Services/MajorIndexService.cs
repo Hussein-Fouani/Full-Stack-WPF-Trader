ec51a6e8720a0e3ab03935710173478cb7ab1794
Hussein <41930483@students.liu.edu.lb>
2024-03-27 13:26:00 +0200

    Add MajorIndexService class to retrieve major index data

---
+++ Trader.API/Services/MajorIndexService.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trader.Domain.Models;
+using Trader.Domain.Services;
+
+namespace Trader.API.Services
+{
+    public class MajorIndexService : IMajorIndex
+    {
+        public async Task<MajorIndex> GetMajorIndex(MajorIndexType indexType)
+        {
+          string API_REQUEST_URI = $"https://financialmodelingprep.com/api/v3/majors-indexes/{GetIndexSuffix(indexType)}/?apikey=3f119eff2e824388d72a1eaa20044775";
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage message = await client.GetAsync(API_REQUEST_URI);
+                    message.EnsureSuccessStatusCode();
+                    if (message.IsSuccessStatusCode)
+                    {
+                        var response = await message.Content.ReadAsStringAsync();
+                        MajorIndex majorIndex = JsonConvert.DeserializeObject<MajorIndex>(response);
+                        majorIndex.Type= indexType;
+                        return majorIndex;
+                    }
+                    else
+                    {
+                        throw new Exception("Cannot retrieve Major Index");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+        private string GetIndexSuffix(MajorIndexType indexType)
+        {
+            return indexType switch
+            {
+                MajorIndexType.DowJones => ".DJI",
+                MajorIndexType.Nasdaq => ".IXIC",
+                MajorIndexType.SP500 => ".GSPC",
+                _ => throw new Exception("Index type not supported"),
+            };
+        }
+    }
+}
