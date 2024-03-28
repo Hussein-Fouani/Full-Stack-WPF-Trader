1333a29ab3feaab1c4ec1c9678d2ed2a92f5d983
Hussein <41930483@students.liu.edu.lb>
2024-03-28 17:05:14 +0200

    Add EqualValuetoParameter converter class

---
+++ TraderWpf/Converts/EqualValuetoParameter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace TraderWpf.Converts
+{
+    public class EqualValuetoParameter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value.ToString().Equals(parameter.ToString());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value.Equals(true) ? parameter : Binding.DoNothing;
+        }
+    }
+    
+      
+    
+}
